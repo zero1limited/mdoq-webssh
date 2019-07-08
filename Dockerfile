ea25c892a5f57c6964b3e2de40631fe0b83dc22d
adamzero1 <adam.crossland@zero1.co.uk>
2019-07-08 18:37:24 +0100

    allow values to be set from environment configuration

--- Dockerfile
+++ Dockerfile
@@ -2,4 +2,6 @@ FROM python:3.7-slim
 ADD . /code
 WORKDIR /code
 RUN pip install -r requirements.txt
-CMD ["python", "run.py"]
+COPY entrypoint.sh /entrypoint.sh
+
+ENTRYPOINT ["/entrypoint.sh"]
---
+++ entrypoint.sh
@@ -0,0 +1,10 @@
+#!/bin/sh -xe
+
+# Set Variables in template file
+sed -i "s/{{TITLE}}/{{${TITLE}}}/g" webssh/templates/index.html
+sed -i "s/{{INSTANCE_DESCRIPTION}}/{{${INSTANCE_DESCRIPTION}}}/g" webssh/templates/index.html
+sed -i "s/{{HOSTNAME}}/{{${HOSTNAME}}}/g" webssh/templates/index.html
+sed -i "s/{{PORT}}/{{${PORT}}}/g" webssh/templates/index.html
+sed -i "s/{{USERNAME}}/{{${USERNAME}}}/g" webssh/templates/index.html
+
+python run.py
