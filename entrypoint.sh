1ba01fa91785a2e18041a395c834a76a6ce8d9f8
adamzero1 <adam.crossland@zero1.co.uk>
2020-01-05 14:53:25 +0000

    - add dockerize - allow an "INSTANCE_ID" to be passed in, so the ID of the form can be changed (this stops browsers auto-filling hidden fields)

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -xe
 
-dockerize -template webssh/templates/index.html
-dockerize -template webssh/static/js/main.js
+dockerize -template webssh/templates/index.html:webssh/templates/index.html
+dockerize -template webssh/static/js/main.js:webssh/static/js/main.js
 
 RUN_OPTIONS="--port=${PORT} "
 
